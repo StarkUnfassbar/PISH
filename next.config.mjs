0362489e69540ea6ec0b1f14a46508d9a60feabf
Daniil Pleshanov <daniil.pleshanov2005@mail.ru>
2025-08-12 03:50:54 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  trailingSlash: true, // Добавляет `/` в конце URL (опционально)
+  output: "export", // Если делаете статический экспорт (`next export`)
+  skipTrailingSlashRedirect: true, // Отключает редиректы для `/` в конце
+  experimental: {
+    appDir: true, // Если используете App Router (Next.js 13+)
+  },
 };
 
 export default nextConfig;
