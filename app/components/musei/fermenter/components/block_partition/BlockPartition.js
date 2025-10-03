bc9521884a1ea3b47c13c2e74760f89af10894df
Daniil Pleshanov <113170005+StarkUnfassbar@users.noreply.github.com>
2025-10-03 14:51:06 +1000

    update new exhibite

--- app/components/musei/fermenter/components/block_partition/BlockPartition.js
+++ app/components/musei/fermenter/components/block_partition/BlockPartition.js
@@ -5,13 +5,31 @@ import { useState, useEffect } from 'react';
 
 import './block_partition.css';
 
-import blockImg1 from '../../img/block_partition/Bioengineering/1.png';
-import blockImg2 from '../../img/block_partition/Bioengineering/2.jpg';
-import blockImg3 from '../../img/block_partition/Bioengineering/3.jpg';
-import blockImg4 from '../../img/block_partition/Bioengineering/4.jpg';
-import blockImg5 from '../../img/block_partition/Bioengineering/5.jpg';
-import blockImg6 from '../../img/block_partition/Bioengineering/6.jpg';
-import blockImg7 from '../../img/block_partition/Bioengineering/7.png';
+
+import blockImg1 from '../../img/block_partition/bioengineering/1.png';
+import blockImg2 from '../../img/block_partition/bioengineering/2.jpg';
+import blockImg3 from '../../img/block_partition/bioengineering/3.jpg';
+import blockImg4 from '../../img/block_partition/bioengineering/4.jpg';
+import blockImg5 from '../../img/block_partition/bioengineering/5.jpg';
+import blockImg6 from '../../img/block_partition/bioengineering/6.jpg';
+import blockImg7 from '../../img/block_partition/bioengineering/7.png';
+
+import blockImg8 from '../../img/block_partition/industrial_goods/1.jpg';
+import blockImg9 from '../../img/block_partition/industrial_goods/2.jpg';
+import blockImg10 from '../../img/block_partition/industrial_goods/3.jpg';
+import blockImg11 from '../../img/block_partition/industrial_goods/4.png';
+import blockImg12 from '../../img/block_partition/industrial_goods/5.jpg';
+import blockImg13 from '../../img/block_partition/industrial_goods/6.jpg';
+import blockImg14 from '../../img/block_partition/industrial_goods/7.jpg';
+
+import blockImg15 from '../../img/block_partition/food/1.jpg';
+import blockImg16 from '../../img/block_partition/food/2.jpg';
+import blockImg17 from '../../img/block_partition/food/3.jpg';
+import blockImg18 from '../../img/block_partition/food/4.jpg';
+
+import blockImg19 from '../../img/block_partition/science/1.png';
+import blockImg20 from '../../img/block_partition/science/2.jpg';
+import blockImg21 from '../../img/block_partition/science/3.jpg';
 
 // Данные о живых системах для каждого раздела
 const partitionsData = {
@@ -65,45 +83,45 @@ const partitionsData = {
         liveSystems: [
             {
                 id: "industrial_1",
-                title: "Промышленная система 1",
-                description: "Описание промышленной системы 1",
-                image: blockImg1
+                title: "Энтомопатогенные нематоды",
+                description: "Эти микроогранизмы применяются в сельском хозяйстве как биоинсектициды. Они помогают контролировать вредителей без химических средств, что востребовано при производстве экологически чистой продукции.",
+                image: blockImg8
             },
             {
                 id: "industrial_2",
-                title: "Промышленная система 2",
-                description: "Описание промышленной системы 2",
-                image: blockImg1
+                title: "Нитрифицирующие бактерии",
+                description: "Эти бактерии учавствуют в очистке сточных вод, превращая аммиак в нитраты. Они важны для биофильтров на промышленных объектах.",
+                image: blockImg9
             },
             {
                 id: "industrial_3",
-                title: "Промышленная система 3",
-                description: "Описание промышленной системы 3",
-                image: blockImg1
+                title: "Микромицеты",
+                description: "Эти грибы испольщуются для биодеградации пластиков и резины, востребованные в промышленности переработки отходов. Они разлагают важные полимерны, уменьшая экологический след промтоваров.",
+                image: blockImg10
             },
             {
                 id: "industrial_4",
-                title: "Промышленная система 4",
-                description: "Описание промышленной системы 4",
-                image: blockImg1
+                title: "Галофильные бактерии",
+                description: "Эти микроогранизмы выживают в услових высокой солености и применяются в биодобыче редких металлов, таких как литий или уран, из соляных растворов и шахтных вод.",
+                image: blockImg11
             },
             {
                 id: "industrial_5",
-                title: "Промышленная система 5",
-                description: "Описание промышленной системы 5",
-                image: blockImg1
+                title: "Красные вородосли",
+                description: "Красные водоросли служат источником агар-агара, который используется в текстильной промышленности для обработки тканей и в производстве бумаги. Так же его применяют в качестве загустителя в красках и косметике.",
+                image: blockImg12
             },
             {
                 id: "industrial_6",
-                title: "Промышленная система 6",
-                description: "Описание промышленной системы 6",
-                image: blockImg1
+                title: "Метаногенные бактерии",
+                description: "Эти бактерии учавствуют в анаэробном брожении и используются для производства биогаза из органических отходов. Полученный метан применяют в энергетике и отоплении промышленных объектов.",
+                image: blockImg13
             },
             {
                 id: "industrial_7",
-                title: "Промышленная система 7",
-                description: "Описание промышленной системы 7",
-                image: blockImg1
+                title: "Цианобактерии",
+                description: "Это фотосинтезирующие микроогранизмы используются для проиводства биотоплива и биопластика, который применяется в упаковочных материалах. Цианобактерии превращают углекислый газ и солнечный свет в углеводороды, заменяющие ископаемые топлива.",
+                image: blockImg14
             }
         ]
     },
@@ -111,27 +129,27 @@ const partitionsData = {
         liveSystems: [
             {
                 id: "food_1",
-                title: "Пищевая система 1",
-                description: "Описание пищевой системы 1",
-                image: blockImg1
+                title: "Молочнокислые бактерии",
+                description: "Это микроогранизмы используются в производстве йогуртов, кефира и сыра. Они ферментируют лактозу в молочную кислоту, улучшая вкус и хранение продуктов.",
+                image: blockImg15
             },
             {
                 id: "food_2",
-                title: "Пищевая система 2",
-                description: "Описание пищевой системы 2",
-                image: blockImg1
+                title: "Дрожжи",
+                description: "Эти грибы применяются в хлебопекарной и алкогольной промышленности. Дрожжи учавствуют в брожении, производя углекислый газ и алкоголь, что важно для выпечки хлеба, производства пива и вина.",
+                image: blockImg16
             },
             {
                 id: "food_3",
-                title: "Пищевая система 3",
-                description: "Описание пищевой системы 3",
-                image: blockImg1
+                title: "Тауматин",
+                description: "Это растение своим белком таумантином, который используется как натуральный подсластитель. Таумарин в 2000 раз слаще сахара и применяется в производстве низкокаларийных продуктов, конфет и напитков.",
+                image: blockImg17
             },
             {
                 id: "food_4",
-                title: "Пищевая система 4",
-                description: "Описание пищевой системы 4",
-                image: blockImg1
+                title: "Ризобиуим",
+                description: "Эти симбиотические бактерии фиксируют атмосферный азот в корнях бобовых растений, улучшая плодородие почты и урожайность. Их потребность в химических удобрениях и способствует производству экологически чистой продукции.",
+                image: blockImg18
             }
         ]
     },
@@ -139,25 +157,27 @@ const partitionsData = {
         liveSystems: [
             {
                 id: "science_1",
-                title: "Научная система 1",
-                description: "Описание научной системы 1",
-                image: blockImg1
+                title: "Культуры стволовых клеток",
+                description: "Эти культуры активно используются в регенеративной медицине и биомедицинских исследоваеиях. Ученые механизмы дифференцировки и взаимодействия клеток, что позволяет разрабатывать новые подходы к лечению поврежденных тканей и органов.",
+                image: blockImg19
             },
             {
                 id: "science_2",
-                title: "Научная система 2",
-                description: "Описание научной системы 2",
-                image: blockImg1
+                title: "Метанокисляющие бактерии",
+                description: "Эти микроорганизмы изучаются в экологии и микробиологии из-за их способности окислять метан, мощный парниковый газ. Исследование мтанокисляющих бактерий помогает разрабатывать технологии по снижению выбросов метана и улучшению экологической ситуации, а так же способствует понимаю углеродного цикла.",
+                image: blockImg20
             },
             {
                 id: "science_3",
-                title: "Научная система 3",
-                description: "Описание научной системы 3",
-                image: blockImg1
+                title: "Кишечная палочка",
+                description: "Кишечная палочка (Escherichia coli) лучше всего относится к разделу Наука. Она широко используется в биотезнологических и молекулярно-биологических исследованиях. E. coli является модельным организмом для изучения генетики, физиологии и биохимии. Она так же активно применяется для производства рекомбинантных белков, таких как инсулин, и для проведения экспериментов в области генной инженерии и синтетической биологии.",
+                image: blockImg21
             }
         ]
     }
 };
+
+
 
 export default function BlockPartition({ 
     isHidden, 
